ef468138eedc8364dc5387b4f3c65a5070ba8f66
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2023-12-04 14:50:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from flask import Flask, jsonify
 import os
-import os
-import together
 from langchain.chains.conversation.memory import ConversationSummaryMemory
 from langchain.chains import ConversationChain
 from langchain.prompts.prompt import PromptTemplate
-import logging
 from typing import Any, Dict, List, Mapping, Optional
 
 from pydantic import Extra, Field, root_validator
@@ -20,12 +17,6 @@ import together
 import logging
 from typing import Any, Dict, List, Mapping, Optional
 
-from pydantic import Extra, Field, root_validator
-
-from langchain.callbacks.manager import CallbackManagerForLLMRun
-from langchain.llms.base import LLM
-from langchain.llms.utils import enforce_stop_tokens
-from langchain.utils import get_from_dict_or_env
 
 class TogetherLLM(LLM):
     """Together large language models."""
